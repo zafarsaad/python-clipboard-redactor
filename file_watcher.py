0003533a2d925bd2872d28ec925975fd32b9a1c6
Saad Zafar <saadzafar.code@gmail.com>
2024-10-12 23:46:55 -0500

    first draft of file_watcher.py

---
+++ file_watcher.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import time
+import threading
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import subprocess
+
+class ReloadHandler(FileSystemEventHandler):
+    # According to ChatGPT, need this function when instance of ReloadHandler created
+    # Takes the name of the script to monitor
+    def __init__(self, script_to_run): 
+        self.script_to_run = script_to_run # Storing the scipt name in an "instance" variable
+
+    def on_modified(self, event):
+        # Check if the modified file is the one you're watching
+        if event.src_path.endswith(self.script_to_run):
+            print(f"{self.script_to_run} modified! Restarting the scipt...")
+            self.restart_script()
+
+    def restart_script(self):
+        # os.execv set to replace current running process with a new one
+        # sys.executable provides path to Python interpreter
+        os.execv(sys.executable, ['python'] + [self.script_to_run])
+    
+def start_watching(script_to_run):
+    # Here we create instance of ReloadHandler with script name
+    event_handler = ReloadHandler(script_to_run)
+
+    # Now we create an Observer instance to monitor the file system
+    observer = Observer()
+    observer.schedule(event_handler, path='.', recursive=False)
+    observer.start()
+    print(f"Watching {script_to_run} for any changes")
+    return observer
+
+
+# The following only runs if script is executed directly, not imported as module.
+if __name__ == "__main__":
+    script_file = 'python-clipboard-redactor.py'
+    observer = start_watching(script_file)
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join() # Waiting for the observer thread to finish before exiting
+
